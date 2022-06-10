abb99f1846885d2d7d89ec747318f71a94257a13
Emre Gültekin <emregultekin.bm@gmail.com>
2022-06-10 23:12:50 +0300

    [sb-02]-spring-module-examples projesi oluşturuldu.

---
+++ sb-02-spring-module-examples/src/test/java/com/sb/gltkn/SpringModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.gltkn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
